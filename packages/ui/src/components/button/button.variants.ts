5456d4037af96075eec028cd1cb68dbab4750d9a
Trevor Mathiak <tmathiak@gmail.com>
2024-08-05 20:59:11 -0400

    style: update button variants

--- packages/ui/src/components/button/button.variants.ts
+++ packages/ui/src/components/button/button.variants.ts
@@ -1,7 +1,7 @@
 import { cva, type VariantProps } from 'class-variance-authority'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -15,15 +15,23 @@ const buttonVariants = cva(
         ghost: 'hover:bg-accent hover:text-accentForeground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
+      radius: {
+        none: 'rounded-none',
+        sm: 'rounded-sm',
+        md: 'rounded-md',
+        lg: 'rounded-lg',
+        full: 'rounded-full',
+      },
       size: {
-        sm: 'h-9 rounded-md px-3',
-        md: 'h-10 px-4 py-2',
-        lg: 'h-11 rounded-md px-8',
+        sm: 'px-3 py-2',
+        md: 'px-4 py-2.5',
+        lg: 'px-6 py-3',
         icon: 'size-10',
       },
     },
     defaultVariants: {
       variant: 'default',
+      radius: 'md',
       size: 'md',
     },
   },
