e50cf70113c480fde84d8a9a2865cf57168bcae3
Trevor Mathiak <tmathiak@gmail.com>
2024-08-20 05:57:49 -0400

    chore: re-config tailwind colors

---
+++ packages/config-tailwind/src/colors/index.ts
@@ -0,0 +1,42 @@
+import { type ThemeConfig } from '../types'
+
+export const colors: ThemeConfig['colors'] = {
+  background: 'hsl(var(--background))',
+  foreground: 'hsl(var(--foreground))',
+  card: {
+    DEFAULT: 'hsl(var(--card))',
+    foreground: 'hsl(var(--card-foreground))',
+  },
+  popover: {
+    DEFAULT: 'hsl(var(--popover))',
+    foreground: 'hsl(var(--popover-foreground))',
+  },
+  primary: {
+    DEFAULT: 'hsl(var(--primary))',
+    foreground: 'hsl(var(--primary-foreground))',
+  },
+  secondary: {
+    DEFAULT: 'hsl(var(--secondary))',
+    foreground: 'hsl(var(--secondary-foreground))',
+  },
+  muted: {
+    DEFAULT: 'hsl(var(--muted))',
+    foreground: 'hsl(var(--muted-foreground))',
+  },
+  accent: {
+    DEFAULT: 'hsl(var(--accent))',
+    foreground: 'hsl(var(--accent-foreground))',
+  },
+  destructive: {
+    DEFAULT: 'hsl(var(--destructive))',
+    foreground: 'hsl(var(--destructive-foreground))',
+  },
+  border: 'hsl(var(--border))',
+  input: 'hsl(var(--input))',
+  ring: 'hsl(var(--ring))',
+  chart1: 'hsl(var(--chart1))',
+  chart2: 'hsl(var(--chart2))',
+  chart3: 'hsl(var(--chart3))',
+  chart4: 'hsl(var(--chart4))',
+  chart5: 'hsl(var(--chart5))',
+}
