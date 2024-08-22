13c1d6d2609a9e43697b24cbefde3533654bb6a3
Trevor Mathiak <tmathiak@gmail.com>
2024-08-21 20:42:18 -0400

    more storybook updates

--- apps/storybook/tailwind.config.ts
+++ apps/storybook/tailwind.config.ts
@@ -7,6 +7,11 @@ import { type Config } from 'tailwindcss'
 import baseConfig from '@shared/tailwind-config'
 
 export default {
-  content: [...baseConfig.content, '../../packages/*/src/**/*.{ts,tsx}'],
+  content: [
+    ...baseConfig.content,
+    '../../packages/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    './stories/**/*.{js,ts,tsx,jsx}',
+  ],
+  darkMode: ['class', '[data-mode="dark"]'],
   presets: [baseConfig],
 } satisfies Config
