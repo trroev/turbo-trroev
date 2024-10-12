a915fc6289e2e5e3e58a70a46944b9a6af2edef7
Trevor Mathiak <tmathiak@gmail.com>
2024-10-11 20:51:46 -0400

    feat: redirects package

--- apps/payload/src/app/(payload)/api/revalidate/route.ts
+++ apps/payload/src/app/(payload)/api/revalidate/route.ts
@@ -1,6 +1,5 @@
-import type { NextRequest } from 'next/server'
 import { revalidateTag } from 'next/cache'
-import { NextResponse } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const collection = request.nextUrl.searchParams.get('collection')
